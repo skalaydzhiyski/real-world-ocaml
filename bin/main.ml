69b294946053988c8ad501b0b0dd93c7991d7dff
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-05-24 16:49:52 +0100

    latest

--- bin/main.ml
+++ bin/main.ml
@@ -21,8 +21,29 @@ let test_some_array_funcs () =
   let value = some_array.(index) in
   print_endline @@ Int.to_string value
 
-(** [TODO] continue with the following two items.
-    1. example of memoisation for recursive functions.
-    2. example of I/O using In_channel and Out_channel.
+(** memoisation *)
+let memoize m f =
+  let memo_table = Hashtbl.create m in
+  let f' = (fun x -> Hashtbl.find_or_add memo_table x ~default:(fun () -> f x)) in
+  f' (** [memo_table] gets collected only when [f'] gets collected! *)
 
-*)
+
+(** corecursion *)
+let make_rec f_norec =
+  let rec f x = f_norec f x in
+  f
+
+let fib_norec fib x =
+  if Int.(x <= 1) then x
+  else fib (x-1) + fib (x-2)
+
+let fib = make_rec fib_norec
+
+(** corecursion + memoisation *)
+let memo_rec m f_norec x =
+  let fref = ref (fun _ -> assert false) in
+  let fmem = memoize m (fun x -> f_norec !fref x) in
+  fref := fmem;
+  fmem x
+
+(** [TODO] Continue here and analyse the memoisation behaviour of fibonacci. *)
