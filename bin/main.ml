01b080d7e891324f1cc2c18c2b98035260f9b54d
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-06-01 11:41:07 +0100

    latest

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,13 @@
 open Core
+
+(** IO stdin *)
+let print_timezone_message () =
+  printf "Pick a timezone: %!"; (** [%!] is required in the format string to flush the buffer *)
+  match In_channel.(input_line stdin) with
+  | None -> failwith "No timezone provided"
+  | Some timezone ->
+    let zone = Time_unix.Zone.find_exn timezone in
+    let time_string = Time.to_string_abs (Time.now()) ~zone in
+    printf "The time in %s is %s. \n%!" time_string timezone
+
+(** IO files *)
