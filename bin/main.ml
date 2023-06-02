fa98701a4a5cc2dca5b0f29bd46a3fd331c7bb22
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-06-02 14:05:50 +0100

    latest

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,7 @@
 open Core
 
 (** IO stdin *)
-let () =
+let print_timezone_msg () =
   printf "Pick a timezone: %!"; (* `%!` is required in the format string to flush the buffer *)
   match In_channel.(input_line stdin) with
   | None -> failwith "No timezone provided"
@@ -11,3 +11,21 @@ let () =
     printf "The time in %s is %s. \n%!" time_string timezone
 
 (** IO files *)
+let create_number_file filename numbers =
+  let outc = Out_channel.create filename in
+  List.iter numbers
+    ~f:(fun x -> Out_channel.fprintf outc "%d\n" x);
+  Out_channel.close outc
+
+(** [read_lines] here reads the full file *)
+let sum_file_numbers filename =
+  List.fold ~init:0 ~f:(fun a x -> Int.of_string x + a)
+    @@ In_channel.read_lines filename
+
+(** [with_file + fold_lines] is a better approach in case we have bigger data *)
+let sum_file_better filename =
+  In_channel.with_file filename
+    ~f:(fun file ->
+        In_channel.fold_lines file
+          ~init:0
+          ~f:(fun a x -> a + Int.of_string x))
